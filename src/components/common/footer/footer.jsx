1eb1bb4001a1f12decab276e0b051664a0f19047
gstevanato <gstevanato@alpenite.com>
2024-11-04 16:28:54 +0100

    improved header and footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { FaLinkedin, FaGithub } from 'react-icons/fa'; // Import the icons you want
-import logo from '../../../icons/logo.svg'; // Adjust the path to your logo.svg file
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import logo from '../../../icons/logo.svg';
 
 function Footer() {
   return (
     <footer className="bg-primary d-flex flex-wrap justify-content-between align-items-center p-4 border-top">
       <div className="col-md-4 d-flex align-items-center">
         <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
-          <img src={logo} alt="Logo" style={{ width: '50px', height: '50px' }} />
+          <img src={logo} alt="Logo" style={{ width: '70px', height: '70px' }} />
         </a>
         <span className="mb-3 mb-md-0">© 2024 Giovanni Stevanato</span>
       </div>
--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,13 +1,18 @@
-// src/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../icons/logo.svg';
+import profilepic from '../../../icons/profilepic.png';
 
 function Header() {
   return (
     <header className="bg-primary text-white p-3">
       <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container">
-          <Link className="navbar-brand" to="/">My Portfolio</Link>
+          <div className='d-flex align-items-center'>
+            <img src={profilepic} alt="Profile" style={{ width: '70px', height: '70px', borderRadius: '50%', marginLeft: '15px' }}/>
+            <Link className="navbar-brand ms-3 me-0" to="/">Stevanato Giovanni</Link>
+            <img src={logo} alt="Logo" style={{ width: '40px', height: '40px' }} />
+          </div>
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
