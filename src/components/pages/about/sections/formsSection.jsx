33d019d0be7cf22ac00df145ed691800e9918a7e
gstevanato <gstevanato@alpenite.com>
2024-11-06 14:55:59 +0100

    improved custom input file

--- src/components/pages/about/sections/formsSection.jsx
+++ src/components/pages/about/sections/formsSection.jsx
@@ -3,6 +3,8 @@ import { Button, Form } from "react-bootstrap";
 
 function FormsSection() {
   const [validated, setValidated] = useState(false);
+  const [fileName, setFileName] = useState("");
+  const [fileError, setFileError] = useState("");
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -12,6 +14,26 @@ function FormsSection() {
     }
 
     setValidated(true);
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const fileSizeMB = file.size / 1000000;
+      const validExtensions = ["image/jpeg", "image/png", "application/pdf"];
+      if (fileSizeMB > 2.5) {
+        event.target.value = "";
+        setFileName("");
+        setFileError("File size should not exceed 2.5MB");
+      } else if (!validExtensions.includes(file.type)) {
+        event.target.value = "";
+        setFileName("");
+        setFileError("Only PDF, JPEG, and PNG files are allowed");
+      } else {
+        setFileName(file.name);
+        setFileError(""); // Clear error if the file is valid
+      }
+    }
   };
 
   return (
@@ -73,14 +95,30 @@ function FormsSection() {
         </Form.Group>
 
         {/* File Input */}
-        <Form.Group controlId="formFile" className="mt-3">
+        <Form.Group controlId="formFile" className="mt-3 position-relative">
           <Form.Label>File Input</Form.Label>
-          <div className="custom-file-input">
-            <input type="file" id="fileInput" className="file-input" required />
+          <div
+            className={`custom-file-input ${
+              fileError ? "custom-file-input--error" : ""
+            }`}
+          >
+            <input
+              type="file"
+              id="fileInput"
+              className="file-input"
+              accept="image/png, image/jpeg, application/pdf"
+              onChange={handleFileChange}
+              required
+            />
             <label htmlFor="fileInput" className="file-input-label">
-              Choose a file...
+              {fileName ? fileName : "Choose a file..."}
             </label>
+            <div className="position-absolute me-4 end-0 bottom-auto fs-12">
+              <small>Max 2.5MB | JPG, PNG, PDF</small>
+            </div>
           </div>
+          {/* Show error message if file validation fails */}
+          {fileError && <p className="text-danger">{fileError}</p>}
         </Form.Group>
 
         {/* Checkboxes */}
