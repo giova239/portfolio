905585bef5d34c2b9ec88a7bc12fee06969c9410
gstevanato <gstevanato@alpenite.com>
2024-11-09 02:30:56 +0100

    Completed contact cards on contact page

--- src/components/pages/contact/components/contactCard.jsx
+++ src/components/pages/contact/components/contactCard.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-function ContactCard({ icon: Icon, iconColor = "primary", line1, line2 }) {
+function ContactCard({
+  icon: Icon,
+  iconColor = "primary",
+  line1,
+  line2,
+  isLink = false,
+}) {
   return (
     <Card className="bg-dark mb-4">
       <Card.Body className="d-flex align-items-center">
-        <Icon size={50} className={`text-primary me-5 text-${iconColor}`} />
+        <Icon
+          size={50}
+          className={`flex-grow text-primary me-5 text-${iconColor}`}
+        />
         <div className="d-flex flex-column justify-content-center">
-          <p className="mb-0">{line1}</p>
-          <p className="mb-0">{line2}</p>
+          <p className="fs-18 mb-1">{line1}</p>
+          {isLink ? (
+            <a
+              href={line2}
+              className="fs-14 mb-0 text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {line2}
+            </a>
+          ) : (
+            <p className="fs-14 mb-0">{line2}</p>
+          )}
         </div>
       </Card.Body>
     </Card>
--- src/components/pages/contact/contact.jsx
+++ src/components/pages/contact/contact.jsx
@@ -14,52 +14,53 @@ import {
 function Contact() {
   return (
     <div className="contact-page pt-14">
-      <h1 className="text-center">Contact Page</h1>
-      <Row className="mt-4">
-        <Col lg={6} className="mb-4">
-          <Card>
-            <Card.Body>
-              <ContactForm />
-            </Card.Body>
-          </Card>
+      <h1 className="text-center mb-12">Contact Page</h1>
+      <Row className="justify-content-around">
+        <Col lg={6} xl={5} xxl={4} className="mb-4">
+          <h2 className="mb-6">Contact Form:</h2>
+          <ContactForm />
         </Col>
 
-        <Col lg={6}>
+        <Col lg={6} xl={5} xxl={4}>
+          <h2 className="mb-6">Contact informations:</h2>
           <ContactCard
             icon={FaHome}
             iconColor="primary"
-            line1="Residence:"
+            line1="Residence"
             line2="Venice, Italy"
           />
           <ContactCard
             icon={FaPhone}
             iconColor="secondary"
-            line1="Phone:"
-            line2="+1 (234) 567-890"
+            line1="Phone"
+            line2="+39 3456129917"
           />
           <ContactCard
             icon={FaEnvelope}
             iconColor="info"
-            line1="Email:"
+            line1="Email"
             line2="stevanay@gmail.com"
           />
           <ContactCard
             icon={FaLinkedin}
             iconColor="accent1"
-            line1="LinkedIn:"
-            line2="https://www.linkedin.com/in/stevanatogiovanni/"
+            line1="LinkedIn"
+            line2="https://www.linkedin.com/in/stevanatogiovanni"
+            isLink
           />
           <ContactCard
             icon={FaGithub}
             iconColor="accent2"
-            line1="GitHub:"
-            line2="github.com/yourprofile"
+            line1="GitHub"
+            line2="https://github.com/giova239"
+            isLink
           />
           <ContactCard
             icon={FaInstagram}
             iconColor="accent3"
-            line1="Instagram:"
-            line2="instagram.com/yourprofile"
+            line1="Instagram"
+            line2="https://www.instagram.com/giobba___"
+            isLink
           />
         </Col>
       </Row>
